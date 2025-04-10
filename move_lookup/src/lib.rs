0c85714b1d4b0e1e00e442ec5580a24e3f93f774
Nek <NekSaikou@proton.me>
2025-04-10 13:47:33 +0700

    cleanup

--- move_lookup/src/lib.rs
+++ move_lookup/src/lib.rs
@@ -1,62 +1,55 @@
-#![allow(long_running_const_eval)]
-pub static MOVE_TABLE: [u32; 1 << 20] = build_move_lut();
-
-const fn build_move_lut() -> [u32; 1 << 20] {
-    let mut line = [0u32; 4];
+#[allow(long_running_const_eval)]
+pub static MOVE_TABLE: [u32; 1 << 20] = {
     let mut moves = [0; 1 << 20];
     let mut i = 0;
-    while i < 1 << 20 {
-        let mut j = 0;
-        while j < 4 {
-            line[j] = (i as u32 >> (j * 5)) & 0x1f;
-            j += 1;
-        }
+    while i < moves.len() {
+        let mut line = [
+            (i as u32 >> 0) & 0x1f,
+            (i as u32 >> 5) & 0x1f,
+            (i as u32 >> 10) & 0x1f,
+            (i as u32 >> 15) & 0x1f,
+        ];
 
         let mut merged = false;
-        let mut new_x = 3;
-        let mut x = 3;
+        let mut from = 3;
+        let mut to = 3;
         loop {
-            if line[x] == 0 {
-                if x == 0 {
+            if line[from] == 0 {
+                if from == 0 {
                     break;
                 } else {
-                    x -= 1;
+                    from -= 1;
                     continue;
                 }
             }
 
-            if !merged && new_x < 3 && line[x] == line[new_x + 1] {
-                line[new_x + 1] += 1;
-                line[x] = 0;
+            if !merged && to < 3 && line[from] == line[to + 1] {
+                line[to + 1] += 1;
+                line[from] = 0;
                 merged = true;
-            } else if new_x == x {
-                if x == 0 {
+            } else if to == from {
+                if from == 0 {
                     break;
                 } else {
-                    new_x -= 1;
+                    to -= 1;
                 }
             } else {
-                line[new_x] = line[x];
-                line[x] = 0;
+                line[to] = line[from];
+                line[from] = 0;
                 merged = false;
-                new_x -= 1;
+                to -= 1;
             }
 
-            if x == 0 {
+            if from == 0 {
                 break;
             } else {
-                x -= 1;
+                from -= 1;
             }
         }
 
-        moves[i] = 0;
-        let mut j = 0;
-        while j < 4 {
-            moves[i] += line[j] << (j * 5);
-            j += 1
-        }
+        moves[i] = (line[0] << 0) | (line[1] << 5) | (line[2] << 10) | (line[3] << 15);
 
         i += 1;
     }
     moves
-}
+};
